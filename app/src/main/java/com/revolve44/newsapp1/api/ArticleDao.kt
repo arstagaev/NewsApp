70cc8fdb0a521da5a46bbf8a77a336e34b9ac6ff
ArsMars <tag_mahal@mail.ru>
2020-10-06 21:39:50 +0300

    fix bugs

--- app/src/main/java/com/revolve44/newsapp1/api/ArticleDao.kt
+++ app/src/main/java/com/revolve44/newsapp1/api/ArticleDao.kt
@@ -3,10 +3,11 @@ package com.revolve44.newsapp1.api
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import com.revolve44.newsapp1.models.Article
 
 
 @Dao
 interface ArticleDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun upsert(article:)
+    suspend fun upsert(article: Article)
 }
